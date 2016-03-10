0508a2d76f141e34edf11c8453e33cd2922ec47b
stevula <brodericksteven@gmail.com>
2016-03-09 16:18:08 -0800

    Fix tile-rendering and add game loop/ win condition

--- game.rb
+++ game.rb
@@ -5,6 +5,7 @@ class Game
   def initialize
     @guesses_remaining = 7
     @word = get_word
+    @letters = @word.split(//)
     run
   end
 
@@ -17,24 +18,58 @@ class Game
 
   def run
     build_letters
-    render_tiles
+
+    until game.won?
+      render_tiles
+      prompt_for_guess
+
+      if @guesses_remaining == 0
+        puts "You died."
+        break
+      end
+    end
+
+    puts "Play again? (Y/N)"
   end
 
   def build_letters
-    letter_strings = @word.split(//).uniq
+    unique_letters = @letters.uniq
     
-    @letter_pairs = letter_strings.map do |letter_value|
+    # this becomes a collection of letters and letter objects {"A" => LetterObject}
+    @letter_pairs = {}
+
+    unique_letters.each do |letter_value|
       letter_object = Letter.new(letter_value)
-      {letter_value => letter_object}
+      @letter_pairs[letter_value] = letter_object
     end
   end
 
   def render_tiles
     @letters.each do |letter|
-      letter.guessed? ?  print "_#{letter}_" : print "___"
+      letter_object = @letter_pairs[letter]
+      puts letter_object.guessed? ? "_#{letter}_" : "___"
     end
   end
 
-  def guess
+  def prompt_for_guess
+    puts "Which letter would you like to guess?"
+    letter = gets.chomp
+    guess(letter)
+  end
+
+  def guess(letter_value)
+    letter = @letter_pairs[letter_value]
+
+    if letter
+      puts "Correct!"
+      letter.mark_guessed
+    else
+      puts "Incorrect guess!"
+      @guesses_remaining -= 1
+    end
+  end
+
+  def won?
+    @letter_pairs.all? {|letter, letter_obj| letter.obj.guessed?}
   end
 end
